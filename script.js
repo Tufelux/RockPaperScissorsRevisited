b9b2140bd5e82c5acde84e0e77c01095cdec7bb7
Tufelux <126290164+Tufelux@users.noreply.github.com>
2026-09-14 18:49:34 +0100

    game is now playable and works I think

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
-
-let computerScore = 0;
-let humanScore = 0;
-
-function getComputerChoice() {
+function getCPUChoice() {
     let rnd = Math.floor(Math.random() * 3);
     
-    if (rnd == 0) {
-        return "Rock"
+    if (rnd === 0) {
+        return "rock";
     }
-    else if (rnd == 1) {
-        return "Paper"
+    else if (rnd === 1) {
+        return "paper";
     }
     else {
-        return "Scissors"
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    let choice = prompt("Choose your move: ")
+    let choice = prompt("Choose your move: ");
 
-    return console.log(choice)
+    return choice.toLowerCase();
 }
 
-function playRound(computerChoice, humanChoice) {
-    
+function playRound(humanChoice, cpuChoice) {
+
+    if (humanChoice === cpuChoice) {
+        return "draw";
+    }
+
+    if (
+        (humanChoice === "rock" && cpuChoice === "scissors") ||
+        (humanChoice === "paper" && cpuChoice === "rock") ||
+        (humanChoice === "scissors" && cpuChoice === "paper")
+    ) {
+        return "human";
+    }
+    return "cpu";
 }
 
-getHumanChoice();
+function playGame() {
 
-// getComputerChoice()
+    let humanScore = 0;
+    let cpuScore = 0;
+
+    while (humanScore !== 5 && cpuScore !== 5) {
+
+        let result = playRound(getHumanChoice(), getCPUChoice())
+             
+        if (result === "human") {
+            humanScore++;
+            console.log(`Human wins this round. Human: ${humanScore}, CPU: ${cpuScore}`);
+        }
+        else if (result === "cpu") {
+            cpuScore++;
+            console.log(`CPU wins this round. Human: ${humanScore}, CPU: ${cpuScore}`);
+        }
+        else {
+            console.log(`It's a draw. Human: ${humanScore}, CPU: ${cpuScore}`);
+        }
+    }
+
+    if (humanScore >= 5) {
+        return "The Human Wins";
+    }
+    else {
+        return "The CPU Wins";
+    }
+}
+
+console.log(playGame());
